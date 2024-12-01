6a9cad9cf1be904ae6ef5cbbc28b2270bd588d01
utkinapopka <utkinapopka@gmail.com>
2024-12-01 22:40:38 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,13 @@
+print('1st program');
+print(9**0.5*5);
+
+print('2nd program');
+print (bool((9.99 > 9.98) and (1000 != 1000.1)));
+
+print('3rd program');
+print(2*2+2);
+print(2*(2+2));
+print(bool(2*2+2==2*(2+2)));
+
+print('4th program');
+print(int(123.456*10)%10);
